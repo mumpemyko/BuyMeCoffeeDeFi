d6bae08d29e934b0b70be900cdb0eeb5b97e278d
mumpe <mumpemyko22@gmail.com>
2022-10-26 19:53:54 +0300

    initial commit

---
+++ scripts/withdraw.js
@@ -0,0 +1,52 @@
+// scripts/withdraw.js
+
+const hre = require("hardhat");
+const abi = require("../artifacts/contracts/BuyMeACoffee.sol/BuyMeACoffee.json");
+
+async function getBalance(provider, address) {
+  const balanceBigInt = await provider.getBalance(address);
+  return hre.ethers.utils.formatEther(balanceBigInt);
+}
+
+async function main() {
+  // Get the contract that has been deployed to Goerli.
+  const contractAddress="0xb5D94C7d144D24fAE0ef699AB0c92B9E33e9B79a";
+  const contractABI = abi.abi;
+
+  // Get the node connection and wallet connection.
+  const provider = new hre.ethers.providers.AlchemyProvider("goerli", process.env.GOERLI_API_KEY);
+
+  // Ensure that signer is the SAME address as the original contract deployer,
+  // or else this script will fail with an error.
+  const signer = new hre.ethers.Wallet(process.env.PRIVATE_KEY, provider);
+  const newOwner = "0x643634ed2092a8999e00127EAddc9cD718BAfACC";
+
+  // Instantiate connected contract.
+  const buyMeACoffee = new hre.ethers.Contract(contractAddress, contractABI, signer);
+
+  // Check starting balances.
+  console.log("current balance of owner: ", await getBalance(provider, signer.address), "ETH");
+  const contractBalance = await getBalance(provider, buyMeACoffee.address);
+  console.log("current balance of contract: ", await getBalance(provider, buyMeACoffee.address), "ETH");
+
+  // Withdraw funds if there are funds to withdraw.
+  if (contractBalance !== "0.0") {
+    console.log("withdrawing funds..")
+    const withdrawTxn = await buyMeACoffee.withdrawTips();
+    await withdrawTxn.wait();
+  } else {
+    console.log("no funds to withdraw!");
+  }
+
+  // Check ending balance.
+  console.log("current balance of newowner: ", await getBalance(provider, newOwner), "ETH");
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
